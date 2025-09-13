c115cef5b0f9e3183cf88634811ebaa9368d3dbe
Amna Tahir <amntahir1@gmail.com>
2025-09-13 18:51:05 +0500

    test: add 2 Espresso smoke tests and androidTest deps

---
+++ app/src/androidTest/java/com/amnatahir/i229885/BottomBarTest.kt
@@ -0,0 +1,25 @@
+﻿package com.amnatahir.i229885
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class BottomBarTest {
+    @get:Rule
+    val rule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun click_bottom_buttons_does_not_crash() {
+        onView(withId(R.id.btnt1)).perform(click())
+        onView(withId(R.id.btnt2)).perform(click())
+        onView(withId(R.id.btnt3)).perform(click())
+        onView(withId(R.id.btnt4)).perform(click())
+        onView(withId(R.id.btnt5)).perform(click())
+    }
+}
---
+++ app/src/androidTest/java/com/amnatahir/i229885/NavSmokeTest.kt
@@ -0,0 +1,19 @@
+﻿package com.amnatahir.i229885
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import com.google.common.truth.Truth.assertThat
+
+@RunWith(AndroidJUnit4::class)
+class NavSmokeTest {
+    @get:Rule
+    val rule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun app_launches() {
+        assertThat(true).isTrue()
+    }
+}
